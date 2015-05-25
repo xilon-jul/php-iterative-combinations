9a62c8c371d9779ced0617d13dc8c2e44f7502ff
julien.pons <julien.pons@mobpartner.com>
2015-05-25 16:17:30 +0200

    Compat php 5.3

--- src/Algorithm/Combination.php
+++ src/Algorithm/Combination.php
@@ -9,7 +9,7 @@ abstract class Combination {
         
         $nbEntries = count($entries);
         // Join trees by adding a common root
-        $flattenArray[0] = ['value' => '', 'level' => 0, 'next' => 1, 'offset' => 0, 'next_sibling' => null];
+        $flattenArray[0] = array('value' => '', 'level' => 0, 'next' => 1, 'offset' => 0, 'next_sibling' => null)
         $flattenOffset = 1;
         for($i=0;$i<$nbEntries;$i++){
             // An entry is a final array which contains elements
@@ -17,7 +17,7 @@ abstract class Combination {
             for($j=0;$j<$nbElements;$j++){
                 $nextOffset = ($i === $nbEntries-1) ? null : $flattenOffset + $nbElements - $j;
                 $nextSibling = ($nbElements-1 !== $j) ? ($flattenOffset + 1) : null;
-                $flattenArray[$flattenOffset++] = ['offset' => $flattenOffset, 'value' => $entries[$i][$j], 'next' => $nextOffset, 'next_sibling' => $nextSibling];
+                $flattenArray[$flattenOffset++] = array('offset' => $flattenOffset, 'value' => $entries[$i][$j], 'next' => $nextOffset, 'next_sibling' => $nextSibling);
             }
         }
         return $flattenArray;
@@ -36,9 +36,9 @@ abstract class Combination {
         if(isset($cache[$hashcode])){
             return $cache[$hashcode];
         }
-        $combinations = [];
+        $combinations = array();
         // Traverse n-ary tree from left to right in-order (current node first)
-        $stack = [];
+        $stack = array();
         $node = $flattenArray[0];
         $done = 0;
         // Traversal callbacks
